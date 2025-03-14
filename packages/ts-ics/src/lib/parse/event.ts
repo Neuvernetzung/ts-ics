5cf2f05cbc18558e1e376ab1f7e18a5d69fbf5b5
Tim Heerwagen <t.heerwagen@web.de>
2025-03-14 16:40:10 +0100

    resolve merge conflict

--- packages/ts-ics/src/lib/parse/event.ts
+++ packages/ts-ics/src/lib/parse/event.ts
@@ -82,11 +82,6 @@ export const convertIcsEvent = <T extends NonStandardValuesGeneric>(
       return;
     }
 
-    if (objectKey === "recurrenceId") {
-      set(event, objectKey, icsRecurrenceIdToObject(value, options, timezones));
-      return;
-    }
-
     if (objectKey === "duration") {
       event[objectKey] = convertIcsDuration(undefined, line);
       return;
