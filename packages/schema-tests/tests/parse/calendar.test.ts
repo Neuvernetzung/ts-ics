41a08e88b5c7b80602108d654590149f2e5ae097
Tim Heerwagen <35360306+timheerwagen@users.noreply.github.com>
2025-05-28 16:30:19 +0200

    Add missing properties to zIcsCalendar schema

--- packages/schema-tests/tests/parse/calendar.test.ts
+++ packages/schema-tests/tests/parse/calendar.test.ts
@@ -46,6 +46,70 @@ describe("Tests if all values from IcsCalendar are parsed from schema package", 
       "DESCRIPTION:Mozilla Standardbeschreibung",
       "END:VALARM",
       "END:VEVENT",
+      "BEGIN:VFREEBUSY",
+      "UID:19970901T095957Z-76A912@example.com",
+      "ORGANIZER:mailto:jane_doe@example.com",
+      "ATTENDEE:mailto:john_public@example.com",
+      "DTSTAMP:19970901T100000Z",
+      "DTSTART:19970901T100000Z",
+      "DTEND:19980901T100000Z",
+      "FREEBUSY:19971015T050000Z/PT8H30M,",
+      " 19971015T160000Z/PT5H30M,19971015T223000Z/PT6H30M",
+      "URL:http://example.com/pub/busy/jpublic-01.ifb",
+      "COMMENT:This iCalendar file contains busy time information for",
+      "  the next three months.",
+      "END:VFREEBUSY",
+      "BEGIN:VJOURNAL",
+      "UID:19970901T130000Z-123403@example.com",
+      "DTSTAMP:19970901T130000Z",
+      "DTSTART;VALUE=DATE:19971102",
+      "CREATED;VALUE=DATE:19971102",
+      "LAST-MODIFIED;VALUE=DATE:19971102",
+      "URL:https://test.de",
+      "EXDATE:20070402T010000Z",
+      "SUMMARY:Our Blissful Anniversary",
+      "DESCRIPTION:Some description.",
+      "COMMENT:Some comment.",
+      "LOCATION:Unknown place",
+      "GEO:48.85299;2.36885",
+      `ORGANIZER;CN="Alice Balder, Example Inc.":MAILTO:alice@example.com`,
+      "PRIORITY:5",
+      "SEQUENCE:1",
+      "STATUS:DRAFT",
+      "ATTACH;FMTTYPE=audio/basic:ftp://example.com/pub/sounds/bell-01.aud",
+      `ATTENDEE;MEMBER="mailto:DEV-GROUP@example.com":mailto:joecool@example.com`,
+      "RECURRENCE-ID;VALUE=DATE:19960401",
+      "CLASS:CONFIDENTIAL",
+      "CATEGORIES:ANNIVERSARY,PERSONAL,SPECIAL OCCASION",
+      "RRULE:FREQ=YEARLY",
+      "END:VJOURNAL",
+      "BEGIN:VTODO",
+      "UID:19970901T130000Z-123403@example.com",
+      "DTSTAMP:19970901T130000Z",
+      "DTSTART;VALUE=DATE:19971102",
+      "CREATED;VALUE=DATE:19971102",
+      "LAST-MODIFIED;VALUE=DATE:19971102",
+      "URL:https://test.de",
+      "EXDATE:20070402T010000Z",
+      "DURATION:P1D",
+      "SUMMARY:Our Blissful Anniversary",
+      "DESCRIPTION:Some description.",
+      "COMMENT:Some comment.",
+      "LOCATION:Unknown place",
+      "COMPLETED:19970901T130000Z",
+      "PERCENT-COMPLETE:80",
+      "GEO:48.85299;2.36885",
+      `ORGANIZER;CN="Alice Balder, Example Inc.":MAILTO:alice@example.com`,
+      "PRIORITY:5",
+      "SEQUENCE:1",
+      "STATUS:NEEDS-ACTION",
+      "ATTACH;FMTTYPE=audio/basic:ftp://example.com/pub/sounds/bell-01.aud",
+      `ATTENDEE;MEMBER="mailto:DEV-GROUP@example.com":mailto:joecool@example.com`,
+      "RECURRENCE-ID;VALUE=DATE:19960401",
+      "CLASS:CONFIDENTIAL",
+      "CATEGORIES:ANNIVERSARY,PERSONAL,SPECIAL OCCASION",
+      "RRULE:FREQ=YEARLY",
+      "END:VTODO",
       "END:VCALENDAR",
     ]);
 
@@ -60,6 +124,17 @@ describe("Tests if all values from IcsCalendar are parsed from schema package", 
             `The calendar does not contain the value "${calendarKey}".`
           );
       });
+
+      if (!calendar.timezones)
+        throw new Error("The calendar does not contain timezones.");
+      if (!calendar.events)
+        throw new Error("The calendar does not contain events.");
+      if (!calendar.todos)
+        throw new Error("The calendar does not contain todos.");
+      if (!calendar.journals)
+        throw new Error("The calendar does not contain journals.");
+      if (!calendar.freeBusy)
+        throw new Error("The calendar does not contain freeBusy.");
     });
   });
 });
